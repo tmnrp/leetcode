b1c49be73ee24a9c252a6b365267392c1ffc06ec
Harsh Parammal <harsh.parammal@brain2.com>
2025-10-14 10:01:39 +0530

    valid anagram

---
+++ 242-valid-anagram/242-valid-anagram-01.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function (s, t) {
+  if (s.length !== t.length) {
+    return false;
+  }
+
+  const arr = new Array(26).fill(0);
+  for (let i = 0; i < s.length; i++) {
+    arr[s[i].charCodeAt(0) - "a".charCodeAt(0)]++;
+    arr[t[i].charCodeAt(0) - "a".charCodeAt(0)]--;
+  }
+
+  return arr.some((v) => v !== 0) ? false : true;
+};
+
+console.log(isAnagram("anagram", "nagaram"));
+console.log(isAnagram("rat", "car"));
